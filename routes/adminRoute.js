29f7185f513242de06e32d34d360e6f87d9551c6
Nithiya5 <nithiyaprabha.r2022@sece.ac.in>
2025-02-16 11:25:31 +0530

    delete

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login,registerEmployee } = require('../controllers/adminController'); // Import controller
+const { register, login,registerEmployee,deleteEmployee } = require('../controllers/adminController'); // Import controller
 const router = express.Router();
 
 // Route for registering users (admin, employee, visitor)
@@ -9,5 +9,7 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.post('/registerEmployee',registerEmployee);
+
+router.delete('/deleteEmployee/:employeeId',deleteEmployee);
 
 module.exports = router;
