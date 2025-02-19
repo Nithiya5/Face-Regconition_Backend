fd38c674670c406e6c272128dac2ffc45b6f499e
R Kanishkasree <kanishkasree804@gmail.com>
2025-02-19 11:29:23 +0530

    basic setup

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middlewares/auth");
 const { loginEmployee,forgotPassword,resetPassword,updateFaceEmbeddings } = require('../controllers/employeeController'); // Import the login function
 
 router.post('/login', loginEmployee); // POST request to /login
 router.post('/forgotPassword',forgotPassword);
 router.post('/resetPassword',resetPassword);
-router.put('/updateFaceEmbeddings',updateFaceEmbeddings);
+router.put('/updateFaceEmbeddings',auth,updateFaceEmbeddings);
 
 module.exports = router;
